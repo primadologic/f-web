af78eb981bc07376bad806adcb419e2611f4662d
primado <primadoprincepa@gmail.com>
2025-02-24 19:03:43 +0000

    add header

---
+++ src/features/public/terms/termsHeader.tsx
@@ -0,0 +1,11 @@
+import CustomHeader from "../../../components/header";
+
+
+
+export default function TermsHeader() {
+
+    return (
+        <CustomHeader title="Terms of Use" description="The terms of use for FraudWall" />
+    )
+    
+};
